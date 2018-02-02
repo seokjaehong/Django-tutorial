eaba9b48e3ef40d8ac42ed7b8f4a551c64b7d065
seokjae hong <hsj2334@gmail.com>
2018-02-02 14:08:35 +0900

    was_published_recently() 함수 구현

--- django/polls/models.py
+++ django/polls/models.py
@@ -1,13 +1,23 @@
+import datetime
 from django.db import models
-
-
 # Create your models here.
+from django.utils import timezone
+
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
     def __str__(self):
         return self.question_text
+
+    """
+    이 Question의 게시일자가 현재시각에서 1일전보다 이후인지를 판단해서 리턴
+    :return:
+    """
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
 class Choice(models.Model):
