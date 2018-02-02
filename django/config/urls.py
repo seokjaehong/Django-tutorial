7b8aa7c7f121489b5fa357d09528303ef09a9e4d
seokjae hong <hsj2334@gmail.com>
2018-02-02 14:33:11 +0900

    url추가, view추가

--- django/config/urls.py
+++ django/config/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path,include
 from django.contrib import admin
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls'))
--- django/polls/urls.py
+++ django/polls/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from . import views
 
 urlpatterns =[
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:question_id', views.detail , name ='detail'),
+    path('<int:question_id', views.results , name ='results'),
+    path('<int:question_id', views.vote , name ='vote'),
+
+    # re_path를 사용하여 정규표현식으로 구현하기
+    re_path(r'^$',views.index, name ='index'),
+    re_path(r'^(?P<question_id>\d+)/$', views.detail , name ='detail'),
+    re_path(r'^(?P<question_id>\d+)/results/$',views.results, name='results'),
+    re_path(r'^(?P<question_id>\d+)/vote/$',views.vote, name ='vote')
+
 ]
